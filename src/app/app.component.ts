a64e27f92eed437436c008f41593a902a7fd044b
PetMe <petmesrbija@gmail.com>
2019-01-17 14:29:46 +0100

    Adds header component and scss styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'I am app component';
+  componentTitle = 'I am component title';
+
+  clickHandler() {
+    alert('Hello');
+  }
 }
